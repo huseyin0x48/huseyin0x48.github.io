c74a4f74954fc536b1dc9951c4bcc7ccc66b6a57
huseyin0x48 <huseyin.dev@protonmail.com>
2022-08-28 23:25:28 +0300

    deleted

--- Web-Site-1/assets/js/typewriter.js
+++
@@ -1,6 +0,0 @@
-/* Typewriter */
-const instance = new Typewriter('#typewriter', {
-    strings: ['   ', 'Web Developer', 'Student', 'Robotıc & Securıty Researcher', 'Webmaster'],
-  	autoStart: true,
-  	loop: true,
-});
